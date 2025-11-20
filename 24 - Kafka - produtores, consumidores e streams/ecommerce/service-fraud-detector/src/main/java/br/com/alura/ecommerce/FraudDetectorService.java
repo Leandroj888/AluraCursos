bdcb2791d8759abf38735a1c4e7c4b6aa47cd4d7
Leandro Jorge Junges <leandrojunges@yahoo.com.br>
2025-11-19 22:16:27 -0300

    Nitro-2025-11-19-01

---
+++ 24 - Kafka - produtores, consumidores e streams/ecommerce/service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -0,0 +1,82 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.HashMap;
+
+public class FraudDetectorService {
+    public static void main(String[] args) {
+        var fraudDetectorService = new FraudDetectorService();
+        try(var service = new KafkaService<>(
+                FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudDetectorService::parse,
+                Order.class,
+                new HashMap<>())) {
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, Order> record) {
+        System.out.println("----------------------------------------");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Ordens processadas");
+
+    }
+
+ /*
+    public static void main(String[] args) throws InterruptedException {
+        var consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+
+        while(true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+
+            /-*
+            if (records.isEmpty()) {
+                System.out.println("Não encontrei registros");
+                return;
+            }
+            *-/
+            if (!records.isEmpty()) {
+                System.out.println("Encontrei " + records.count() + " registros");
+
+                for (var record : records) {
+                    System.out.println("----------------------------------------");
+                    System.out.println("Precessing new order, checking for fraud");
+                    System.out.println(record.key());
+                    System.out.println(record.value());
+                    System.out.println(record.partition());
+                    System.out.println(record.offset());
+                    Thread.sleep(5000);
+                }
+                System.out.println("Ordens processadas");
+            }
+        }
+    }
+
+    private static Properties properties() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // Se tiver dois serviços com mesmo grupo as mensagens serão distribuidas entre eles
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+        // Muda o nome do consumer para ficar mais amigavel
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + " - " + UUID.randomUUID().toString());
+        // Auto commit
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+        return props;
+    }
+
+  */
+}
---
+++ 24 - Kafka - produtores, consumidores e streams/ecommerce/service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -0,0 +1,78 @@
+package br.com.alura.ecommerce;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class NewOrderMain {
+    public static void main(String [] args) {
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<Email>()) {
+                for (int i = 0; i < 1; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+                    var body = "Thank you for your order! We are processing your order!";
+                    var email = new Email(userId + "@gmail.com",  body);
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
+            }
+        }
+
+    }
+
+
+    /*
+    public static void main(String [] args) {
+        try (var dispatcher = new KafkaDispatcher()) {
+
+            for (int i = 0; i < 1; i++) {
+                String value = "1122234,122114,122355555";
+                var key = UUID.randomUUID().toString();
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                var email = "Thank you for your order! We are processing your order!";
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+            }
+        }
+
+    }
+
+    /*
+    public static void main(String [] args) throws ExecutionException, InterruptedException {
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties());
+
+        for (int i = 0; i < 1; i++) {
+            String value = "1122234,122114,122355555";
+
+            var key = UUID.randomUUID().toString();
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+            // .get() força um retorno se conseguiu ou não enviar a mensagem
+            producer.send(record, getCallback()).get();
+
+            var email  = "Thank you for your order! We are processing your order!";
+            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+            producer.send(emailRecord, getCallback()).get();
+        }
+    }
+
+    private static Callback getCallback() {
+        return (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("sucesso enviando " + data.topic() + ":::: partition:" + data.partition() + "/ offset:" + data.offset() + "/ timestamp:" + data.timestamp());
+        };
+    }
+
+    private static Properties properties() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return props;
+    }
+     */
+}
