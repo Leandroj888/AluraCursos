4a7f7b19e1902001a238b28222df702b3c3e999e
Leandro Jorge Junges <leandrojunges@yahoo.com.br>
2026-02-13 14:24:27 -0300

    Nitro-2026-02-13-04

--- 33_Integracao_Continua_Pipeline_Docker_no_Github_Actions/database/db.go
+++ 33_Integracao_Continua_Pipeline_Docker_no_Github_Actions/database/db.go
@@ -14,7 +14,7 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
+	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
